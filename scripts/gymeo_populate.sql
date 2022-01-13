8a1c9c140781a96874cb46e97d2ff9665ef35629
loicrheig <loicrossetmp@gmail.com>
2022-01-13 09:58:19 +0100

    Début de la population de la base de données

--- scripts/gymeo_populate.sql
+++ scripts/gymeo_populate.sql
@@ -1,9 +1,36 @@
 set client_encoding to 'UTF8';
 
-/* Pays */
+/* Utilisateur */
 INSERT INTO Utilisateur (pseudonyme, dateNaissance) VALUES 
 ('Valentin', '1997-11-15'),
 ('Alexandre', '2000-03-19'),
 ('Loïc', '2000-02-09');
+
+/* Programme */
+INSERT INTO Programme (nom, idUtilisateur) VALUES
+('full-body', 1),
+('Zoumba training', 2),
+('Haut du corps', 2),
+('Endurance', 3);
+
+/* Matériel */
+INSERT INTO Matériel (nom, description) VALUES
+('Haltère', 'Idéal pour travailler les bras'),
+('Machine butterfly', 'Efficace pour les pectoraux. Changer la distance du siège en fonction de votre taille'),
+('Machine dos et épaules', ''),
+('Tapis roulant', 'Variez la vitesse en fonction du temps de la séance'),
+('Barre de musculation', 'Le couteau suisse du fitness');
+
+/* Exercice */
+INSERT INTO Exercice (nom, description, nbSériesConseillé, nbRépétitionsConseillé, tempsExécutionConseillé, difficulté, idMatériel) VALUES
+('Dumbell alternate hammer curl', 'Consiste à soulever des haltères en alternant bras gauche/droit. Effectuez cette exercice debout et droit.', 3, 12, null, 'facile', 1),
+('Fly', 'Ecartez les bras afi d''atteindre un angle de 180° entre vos bras et votre torse.', 4, 10, null, 'moyen', 2),
+('Barbell press', 'Levez les bras au maximum, puis descendez jusqu''à que vos coudes atteignent votre torse.', 2, 15, null, 'facile', 3),
+('Back fortification', '', 4, 8, null, 'difficile', 3),
+('Sprint', 'Effectuez des séquences de sprints de 15 secondes alternées par 15 secondes de marche', 3, null, 5, 'difficile', 4),
+('Abdominaux au sol', '', 3, 10, null, 'facile', null);
+
+/* Groupement musculaire */
+
 
 -- TODO
