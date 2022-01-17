d71b5091b593ee48472ed42210cac7348a2acb02
loicrheig <loicrossetmp@gmail.com>
2022-01-17 15:38:37 +0100

    Script d'insertion de valeurs

--- scripts/gymeo_populate.sql
+++ scripts/gymeo_populate.sql
@@ -21,6 +21,24 @@ INSERT INTO Matériel (nom, description) VALUES
 ('Tapis roulant', 'Variez la vitesse en fonction du temps de la séance'),
 ('Barre de musculation', 'Le couteau suisse du fitness');
 
+/* Groupement musculaire */
+INSERT INTO GroupementMusculaire (nom) VALUES
+('Ceinture scapulaire'),
+('Membres supérieurs'),
+('Membres inférieurs'),
+('Paroi abdominale'),
+('Paroi dorsale');
+
+/* Matériel - Groupement musculaire */
+INSERT INTO Matériel_GroupementMusculaire (idMatériel, nomGroupementMusculaire) VALUES
+(1, 'Ceinture scapulaire'),
+(2, 'Ceinture scapulaire'),
+(2, 'Membres supérieurs'),
+(3, 'Ceinture scapulaire'),
+(4, 'Membres inférieurs'),
+(4, 'Paroi abdominale'),
+(5, 'Ceinture scapulaire');
+
 /* Exercice */
 INSERT INTO Exercice (nom, description, nbSériesConseillé, nbRépétitionsConseillé, tempsExécutionConseillé, difficulté, idMatériel) VALUES
 ('Dumbell alternate hammer curl', 'Consiste à soulever des haltères en alternant bras gauche/droit. Effectuez cette exercice debout et droit.', 3, 12, null, 'Facile', 1),
@@ -30,7 +48,57 @@ INSERT INTO Exercice (nom, description, nbSériesConseillé, nbRépétitionsCons
 ('Sprint', 'Effectuez des séquences de sprints de 15 secondes alternées par 15 secondes de marche', 3, null, 5, 'Difficile', 4),
 ('Abdominaux au sol', '', 3, 10, null, 'Facile', null);
 
-/* Groupement musculaire */
+/* Exercice - Groupement musculaire */
+INSERT INTO Exercice_GroupementMusculaire (nomExercice, nomGroupementMusculaire) VALUES
+('Dumbell alternate hammer curl', 'Membres supérieurs'),
+('Fly', 'Ceinture scapulaire'),
+('Fly', 'Membres supérieurs'),
+('Barbell press', 'Ceinture scapulaire'),
+('Barbell press', 'Membres supérieurs'),
+('Back fortification', 'Paroi dorsale'),
+('Sprint', 'Membres inférieurs'),
+('Abdominaux au sol', 'Paroi abdominale');
 
+/* Séance */
+INSERT INTO Séance (dateDébut, dateFin, idProgramme) VALUES
+('2020-05-14','2020-05-15',1),
+('2020-03-14','2020-03-15',1),
+('2020-03-14','2020-03-15',2);
 
+/* Série */
+INSERT INTO Série (nbrépétitions, tempsexécution, poids, idSéance, nomExercice) VALUES
+(5, null, 20, 1, 'Dumbell alternate hammer curl'),
+(10, null, 15, 1, 'Dumbell alternate hammer curl'),
+(null, 30, null, 1, 'Sprint'),
+(28, null, 45, 3, 'Back fortification'),
+(20, null, 45, 3, 'Back fortification'),
+(15, null, 45, 3, 'Back fortification'),
+(30, null, null, 3, 'Abdominaux au sol');
+
+/* Programme - Exercice*/
+INSERT INTO Programme_Exercice (nomExercice, idProgramme, tempsPause, nbSéries, ordre) VALUES
+('Dumbell alternate hammer curl', 1, 30, 5, 1),
+('Sprint', 1, 60, 3, 2),
+('Back fortification', 2, 30, 5, 1),
+('Abdominaux au sol', 2, 60, 3, 2);
+
+/* Lieu */
+INSERT INTO Lieu (nom) VALUES
+('Maison'),
+('Extérieur'),
+('Salle de fitness');
+
+/* Exercice_Lieu */
+INSERT INTO Exercice_Lieu (nomExercice, nomLieu) VALUES
+('Dumbell alternate hammer curl', 'Maison'),
+('Dumbell alternate hammer curl', 'Salle de fitness'),
+('Fly', 'Salle de fitness'),
+('Barbell press', 'Salle de fitness'),
+('Back fortification', 'Salle de fitness'),
+/* ??? Faire un sprint en ext avec un tapis roulant*/
+('Sprint', 'Extérieur'),
+('Sprint', 'Maison'),
+('Sprint', 'Salle de fitness'),
+('Abdominaux au sol', 'Extérieur'),
+('Abdominaux au sol', 'Maison');
 -- TODO
