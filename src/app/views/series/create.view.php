31dceecae5427215db1fff362e7a803b3966902e
Valentin Kaelin <kalanehd@gmail.com>
2022-01-25 18:47:57 +0100

    feat: some work on frontend for session creation

--- src/app/views/series/create.view.php
+++ src/app/views/series/create.view.php
@@ -1,5 +1,5 @@
 <main>
-    <div class="mt-6 container px-4">
+    <div class="mt-6 container px-4 mx-auto">
         <div class="flex items-center justify-between">
             <h2 class="text-lg font-semibold">Suivi des séries</h2>
             <form action="/series" method="POST" class="flex">
@@ -8,11 +8,7 @@
                 </button>
             </form>
         </div>
-        <?php
-        $i = 0;
-        foreach (array_keys($series) as $serie) :
-        ?>
-            <h2 class="text-lg font-semibold"><?= $serie ?></h2>
+        <?php foreach (array_keys($series) as $serie) : ?>
             <table class="mt-2 bg-white shadow rounded-lg overflow-hidden">
                 <thead>
                     <tr class="font-semibold">
@@ -24,18 +20,31 @@
                     </tr>
                 </thead>
                 <tbody>
-                    <?php
 
-                    ?>
+                </tbody>
+                <tbody>
+                    <?php
+                    $i = 0;
+                    foreach ($exercices as $exercice) : ?>
+                        <tr class="<?= $i % 2 == 0 ? 'bg-gray-100' : '' ?>">
+                            <td class="px-4 py-2 truncate max-w-sm"><?= $exercice['nom'] ?></td>
+                            <!-- TODO: ajouter les autres champs -->
+                            <!-- <td class="px-4 py-2 truncate max-w-sm"><?= 0 ?></td>
+                            <td class="px-4 py-2 truncate max-w-sm">
+                                <button onclick="deleteProgram(<?= $program['id'] ?>)" class="ml-3 hover:text-gray-700 focus:outline-none"><i class="fas fa-trash"></i></button>
+                            </td> -->
+                        </tr>
+                    <?php $i++;
+                    endforeach; ?>
                 </tbody>
             </table>
         <?php $i++;
         endforeach; ?>
         <?php if (count($series) == 0) : ?>
             <h2 class="text-lg font-semibold">Aucune série n'as encore été effectuée</h2>
-
         <?php endif; ?>
-        <form action="/series" method="POST">
+        <h2 class="mt-8 text-lg font-semibold">Ajouter une série</h2>
+        <form action="/series" method="POST" class="max-w-md">
             <label class="text-sm font-medium text-gray-700" for="exercice">Exercice</label>
             <select class="ml-2 pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md" id="exercice" name="exercice">
                 <option value="-1">-- Non selectionné</option>
@@ -45,7 +54,7 @@
                     </option>
                 <?php endforeach; ?>
             </select>
-            <div>
+            <div class="mt-2">
                 <label class="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2" for="repetition">
                     repetitions
                 </label>
@@ -65,7 +74,7 @@
             </div>
             <input hidden name="idSession" value="<?= $_GET['idSession'] ?>">
 
-            <button class="ml-4 flex-shrink-0 shadow bg-gray-800 hover:bg-gray-900 focus:outline-none text-white text-sm font-bold py-2 px-4 rounded" type="submit">
+            <button class="mt-2 flex-shrink-0 shadow bg-gray-800 hover:bg-gray-900 focus:outline-none text-white text-sm font-bold py-2 px-4 rounded" type="submit">
                 Série terminée
             </button>
         </form>
