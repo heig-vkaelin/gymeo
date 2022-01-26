c98a7b493e51f2fc7f0525b42b79958eabf303e7
Valentin Kaelin <kalanehd@gmail.com>
2022-01-26 19:12:55 +0100

    feat: add favicon

--- src/app/views/partials/header.php
+++ src/app/views/partials/header.php
@@ -17,6 +17,7 @@
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
     <link href="https://unpkg.com/@tailwindcss/forms/dist/forms.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+    <link rel="icon" type="image/png" href="https://i.imgur.com/Yu2jOZL.png" />
     <style>
         * {
             font-family: 'Inter', sans-serif;
