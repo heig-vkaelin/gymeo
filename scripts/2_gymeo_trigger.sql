450cfa6017d9851acd0de45a885b9733f532092c
AJaquier <alex.jaquier@gmail.com>
2022-01-26 16:45:03 +0100

    feat: trigger 4

--- scripts/2_gymeo_trigger.sql
+++ scripts/2_gymeo_trigger.sql
@@ -32,3 +32,37 @@ INSERT
 	Série 
     FOR EACH ROW 
 EXECUTE FUNCTION function_exercice_dans_programme(); 
+
+
+/* -------------------------------------------------------------------------------------- */
+/*  Une fois la séance terminée, il est impossible d’y ajouter une série supplémentaire.  */
+/* -------------------------------------------------------------------------------------- */
+
+CREATE OR REPLACE
+FUNCTION function_vérification_fin_séance() 
+    RETURNS TRIGGER AS 
+$$ 
+BEGIN
+	IF(NEW.idSéance IN (
+		SELECT 
+			Séance.id
+		FROM
+			Séance
+		WHERE
+			Séance.id = NEW.idSéance
+			AND Séance.dateFin IS NOT NULL)) THEN
+			RAISE EXCEPTION 'La séance à laquelle la série est ajoutée est déjà finie';
+	ELSE
+		RETURN NEW;
+	END IF;
+END;
+
+$$ LANGUAGE plpgsql; 
+ 
+CREATE OR REPLACE TRIGGER avant_insertion_série_non_terminée
+    BEFORE
+INSERT
+	ON
+	Série 
+    FOR EACH ROW 
+EXECUTE FUNCTION function_vérification_fin_séance(); 
